12008897ad383ce8a2131889a2af731d75a08734
Kathryn Mercer <kathrynjmercer@gmail.com>
2024-08-13 12:10:24 +0200

    File Creation

---
+++ Cat Pattern Classifier App.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from fastai.vision.all import *
+
+learn = load_learner('export.pkl')
+
+labels = learn.dls.vocab
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+gr.Interface(fn=predict, 
+             inputs='image', 
+             outputs='label',
+             title = 'Cat Coat Pattern Classifier',
+             description = 'A computer vision classifier that can tell you which of the major coat patterns your cat has! Extra fancy coat patterns to follow.',
+             examples = [r'Domestic_cat_sleeping.JPG', r'calico cat.jpg', r'1200px-British_shorthair_cat-3113513.jpg'],
+             interpretation='default').launch(share=True)
